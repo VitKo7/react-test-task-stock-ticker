e2411dd7fc196f18ef8dfa199b4ea90f9a9c9102
Vitaliy Kornienko <Kornienko.Vitaliy@gmail.com>
2021-05-06 19:52:37 +0300

    Update store.js

--- app/store/store.js
+++ app/store/store.js
@@ -3,5 +3,6 @@ import createRootReducer from '../reducers/index';
 
 export const store = createStore(
     createRootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    +window.__REDUX_DEVTOOLS_EXTENSION__ &&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
 );
